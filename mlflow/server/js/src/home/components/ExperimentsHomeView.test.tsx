e856bab5e831be02f2a01983be2b90b2d0774786
rsun19 <robertssun1234@gmail.com>
2026-01-28 10:06:12 -0500

    updated landing page

--- mlflow/server/js/src/home/components/ExperimentsHomeView.test.tsx
+++ mlflow/server/js/src/home/components/ExperimentsHomeView.test.tsx
@@ -41,7 +41,7 @@ describe('ExperimentsHomeView', () => {
 
     expect(screen.getByRole('heading', { level: 3, name: 'Experiments' })).toBeInTheDocument();
     expect(screen.getByText('Create your first experiment')).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'View all' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Go to Experiments' })).toBeInTheDocument();
 
     await userEvent.click(screen.getByRole('button', { name: 'Create experiment' }));
     expect(onCreateExperiment).toHaveBeenCalled();
